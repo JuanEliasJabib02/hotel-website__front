e7f59d96b16a6bc40f004195978cb1783af5830f
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-01-24 16:08:08 -0500

    loading done

--- src/context/RoomContext.jsx
+++ src/context/RoomContext.jsx
@@ -17,6 +17,8 @@ const RoomProvider = ({ children }) => {
 
   const [total, setTotal] = useState(0);
 
+  const [loading, setLoading] = useState(false)
+
   useEffect(() =>   {
     setTotal(Number(adults[0]) + Number(kids[0]));
   }, [adults,kids])
@@ -25,16 +27,22 @@ const RoomProvider = ({ children }) => {
 
   const handleClick = (e) => {
     e.preventDefault();
+    setLoading(true)
     console.log(e)
     const newRooms = roomData.filter(room => {
       return total <= room.maxPerson
     })
-    setRooms(newRooms)
+
+    setTimeout(() => {
+      setRooms(newRooms)
+      setLoading(false)
+    },3000) 
+  
   }
 
 
   return (
-    < RoomContext.Provider value={{rooms, adults, setAdults, kids, setKids, handleClick}}>
+    < RoomContext.Provider value={{rooms, adults, setAdults, kids, setKids, handleClick,loading}}>
       {children}
     </RoomContext.Provider>
   )
