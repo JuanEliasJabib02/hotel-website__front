89a5e27ed78464dc90664e36ecf8078b1997665e
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-01-24 12:36:29 -0500

    check in check out

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -6,7 +6,7 @@ import KidsDropdown from "./KidsDropdown"
 
 const BookForm = () => {
   return (
-    <form className='h-[300px] bg-pink-200 w-full lg:h-[70px]'>
+    <form className='h-[300px]  w-full lg:h-[70px]'>
       <div className='flex flex-col w-full h-full lg:flex-row'>
         <div className='flex-1 border-r'>
           < CheckIn/>
--- src/components/CheckIn.jsx
+++ src/components/CheckIn.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import "../datepicker.css"
 
+import {BsCalendar} from "react-icons/bs"
 const CheckIn = () => {
+  
+  const [startDate, setStartDate] = useState(false)
   return (
-    <div>CheckIn</div>
+    <div className='relative flex items-center justify-end h-full'>
+       {/* icon */}
+      <div className='absolute z-10 pr-8'>
+        <div>
+          < BsCalendar/>
+        </div>
+      </div>
+
+      < DatePicker
+        className='w-full h-full'
+        selected={startDate}
+        placeholderText='Check in'
+        onChange={(date) => setStartDate(date)}
+      />
+    </div>
   )
 }
 
