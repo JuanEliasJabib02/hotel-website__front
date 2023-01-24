4f8fa964ebcece3e94e61a218305bd8228994e64
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-01-24 11:46:23 -0500

    working on hotel details

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -44,6 +44,16 @@ const Room = ({ room }) => {
           </div>
         </div>
       </div>
+      {/* Name  ande*/}
+      <div className='text-center'>
+        <Link to={`/room/${id}`}>
+            <h3 className='h3'>{name}</h3>
+        </Link>
+        <p className='max-w-[300px] mx-auto mb-3 lg:mb-6'>
+            {description.slice(0,56)}
+        </p>
+      </div>
+
     </div>
   )
 }
